715f2c89934ab3ab318a3ef48f176be0d790b091
vasil <medvedev@vasil>
2024-02-03 12:35:13 +0500

    refactoring

---
+++ internal/config/app.go
@@ -0,0 +1,17 @@
+package config
+
+type AppConf struct {
+	AppPort				uint	`mapstructure:"APP_PORT"`
+	DBConnTimeout		uint	`mapstructure:"DB_CONN_TIMEOUT"`
+	//**************external API*****************//
+	NatoinalizeAPI_URL 	string	`mapstructure:"NATIONALITY_API_URL"`
+	GenderizeAPI_URL 	string	`mapstructure:"GENDERIZE_API_URL"`
+	AgifyAPI_URL		string	`mapstructure:"AGIFY_API_URL"`
+	//******************************************//
+	DBHost				string 	`mapstructure:"DB_HOST"`
+	DBPort				string	`mapstructure:"DB_PORT"`
+	DBUser 				string	`mapstructure:"DB_USER"`
+	DBPass				string 	`mapstructure:"DB_PASS"`
+	DBName 				string	`mapstructure:"DB_DBNAME"`
+}
+
---
+++ internal/store/istore.go
@@ -0,0 +1,13 @@
+package store
+
+import (
+	"api/internal/models"
+	"context"
+)
+
+type IStore interface {
+	GetPersonByID(ctx context.Context, id string) (*models.Person,error)
+	CreatePerson(ctx context.Context, person *models.Person)error
+	UpdatePerson(ctx context.Context, person *models.Person) error 
+	DeletePersonByID(ctx context.Context, id string)error
+}
